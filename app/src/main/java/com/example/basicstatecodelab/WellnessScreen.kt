b0a349e813cc3587c2fde82e36ffe94b9be5747d
Aylin Lobato <s23120252@alumnos.itsur.edu.mx>
2026-09-23 20:22:20 -0600

    state hoisting - StatelessCounter y StatefulCounter

--- app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -1,18 +1,9 @@
 package com.example.basicstatecodelab
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
